db72ccbb592d13f94b8100b5d2023e45099df0ef
matthewdau85 <matthew.donovan@alumni.griffithuni.edu.au>
2025-10-18 22:30:18 +1000

    Add Playwright smoke test

--- apgms/playwright.config.ts
+++ apgms/playwright.config.ts
@@ -1,1 +1,19 @@
-﻿export default {};
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  reporter: [['list']],
+  use: {
+    baseURL: process.env.PLAYWRIGHT_BASE_URL ?? 'http://localhost:3000',
+    trace: 'on-first-retry',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
